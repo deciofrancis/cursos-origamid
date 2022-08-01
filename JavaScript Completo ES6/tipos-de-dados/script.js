91f0c63b2cdb45e1c1b36c8c9f4a998ef0526412
Décio Francis <francisdasilva@gmail.com>
2022-08-01 20:46:35 -0300

    Adicionando e modificando arquivos

---
+++ JavaScript Completo ES6/tipos-de-dados/script.js
@@ -0,0 +1,24 @@
+// Declare uma variável contendo uma string
+let team = 'Flamengo';
+
+// Declare uma variável contendo um número dentro de uma string
+let numString = '1';
+
+// Declare uma variável com a sua idade
+let age = 33;
+
+// Declare duas variáveis, uma com seu nome
+let nameAuthor = 'Decio';
+
+// e outra com seu sobrenome e some as mesmas
+let lastName = 'Silva';
+
+let nameFull = `${nameAuthor} ${lastName}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+let newPharse = `It's time`;
+
+console.log(newPharse, nameFull);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nameAuthor);
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# cursos-origamid
+# Origamid Cursos
+
+Repositório destinado aos projetos, anotações, exercidos e certificados dos cursos da Origamid.
+
+Frond End, UX, UI e Design
+
+:white_check_mark: Web Design Completo
+:white_check_mark: Tipografia Avançada
+:white_check_mark: WooCommerce Como CMS
+:white_check_mark: UX Design Heurísticas
+:white_check_mark: UI Design Avançado
+:hourglass: JavaScript Completo ES6
+:white_large_square: React Completo
+:white_large_square: Redux com React
+:white_large_square: Vue.js 2 Completo
