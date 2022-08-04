80e00ba99730181b14f1f66d504d54b5911cb4d7
Décio Francis <francisdasilva@gmail.com>
2022-08-03 23:54:40 -0300

    add exercicios

---
+++ JavaScript Completo ES6/o-que-e-o-dom/script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlPage = window.location.href;
+console.log(urlPage);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementActive = document.querySelector('.ativo');
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
