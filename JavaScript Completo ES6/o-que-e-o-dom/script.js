f038ff96dd6b3bc7821443b3be5653683c57cac7
Decio Francis <francisdasilva@gmail.com>
2022-08-04 00:09:17 -0300

    teste

--- JavaScript Completo ES6/o-que-e-o-dom/script.js
+++ JavaScript Completo ES6/o-que-e-o-dom/script.js
@@ -4,8 +4,12 @@ console.log(urlPage);
 
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
-const elementActive = document.querySelector('.ativo');
+const elementActive = document.querySelector('.activo');
 
 // Retorne a linguagem do navegador
+const languageBrowser = window.navigator.language;
+console.log(languageBrowser);
 
-// Retorne a largura da janela 
+// Retorne a largura da janela
+const largeWindow = window.innerWidth;
+console.log(largeWindow);
